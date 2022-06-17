210f4cb572c520eb888195e72113b9999f6b1190
4goUnited <4goUnited@gmail.com>
2022-06-18 00:38:46 +0500

    Added test for Len() func

--- students_test.go
+++ students_test.go
@@ -2,6 +2,8 @@ package coverage
 
 import (
 	"os"
+	"testing"
+	"time"
 )
 
 // DO NOT EDIT THIS FUNCTION
@@ -17,4 +19,19 @@ func init() {
 }
 
 // WRITE YOUR CODE BELOW
-
+func Test_Len(t *testing.T) {
+	tData := map[string]struct {
+		p People
+		expLen int
+	}{
+		"empty_slice": {[]Person{}, 0},
+		"one_people": {[]Person{{"Ivan", "Ivanov", time.Now()}}, 1},
+		"two_people": {[]Person{{"Ivan", "Ivanov", time.Now()}, {"Sergey", "Alekseev", time.Now()}}, 2},
+		"three_nil_values": {[]Person{Person{}, Person{}, Person{}}, 3}, }
+	for name, tcase := range tData {
+		got := tcase.p.Len()
+		if got != tcase.expLen {
+			t.Errorf("[%s] expected: %d, got %d", name, tcase.expLen, got)
+		}
+	}
+}
