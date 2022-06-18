33559a0d1c7a2a548ac81e878e0a688c2e48cb9c
4goUnited <4goUnited@gmail.com>
2022-06-18 17:54:26 +0500

    Added test for Less() func

--- students_test.go
+++ students_test.go
@@ -6,6 +6,8 @@ import (
 	"time"
 )
 
+var bDate time.Time = time.Date(2000, 1, 1, 12, 0, 0, 0, time.UTC)
+
 // DO NOT EDIT THIS FUNCTION
 func init() {
 	content, err := os.ReadFile("students_test.go")
@@ -28,6 +30,7 @@ func Test_Len(t *testing.T) {
 		"one_people": {[]Person{{"Ivan", "Ivanov", time.Now()}}, 1},
 		"two_people": {[]Person{{"Ivan", "Ivanov", time.Now()}, {"Sergey", "Alekseev", time.Now()}}, 2},
 		"three_nil_values": {[]Person{Person{}, Person{}, Person{}}, 3}, }
+
 	for name, tcase := range tData {
 		got := tcase.p.Len()
 		if got != tcase.expLen {
@@ -35,3 +38,25 @@ func Test_Len(t *testing.T) {
 		}
 	}
 }
+
+func Test_Less(t *testing.T) {
+	tData := map[string]struct {
+		p People
+		expLess bool
+	}{
+		"all_equal": {[]Person{{"Ivan", "Ivanov", bDate}, {"Ivan", "Ivanov", bDate}}, false},
+		"diff_last_name_1": {[]Person{{"Ivan", "Ivanov", bDate}, {"Ivan", "Alekseev", bDate}}, false},
+		"diff_last_name_2": {[]Person{{"Ivan", "Alekseev", bDate}, {"Ivan", "Ivanov", bDate}}, true},
+		"diff_first_name_1": {[]Person{{"Ivan", "Ivanov", bDate}, {"Alex", "Alekseev", bDate}}, false},
+		"diff_first_name_2": {[]Person{{"Alex", "Ivanov", bDate}, {"Ivan", "Alekseev", bDate}}, true},
+		"diff_bDate_name_1": {[]Person{{"Alex", "Ivanov", bDate}, {"Ivan", "Alekseev", bDate.AddDate(1, 0, 0)}}, false},
+		"diff_bDate_name_2": {[]Person{{"Alex", "Ivanov", bDate}, {"Ivan", "Alekseev", bDate.AddDate(-1, 0, 0)}}, true}, }
+
+	for name, tcase := range tData {
+		got := tcase.p.Less(0, 1)
+		if got != tcase.expLess {
+			t.Errorf("[%s] expected: %t, got %t", name, tcase.expLess, got)
+		}
+	}
+}
+
